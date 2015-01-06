f9b503c94217c0008aa642b911457a3ad51767d6
reustonium <andrew.ruestow@gmail.com>
2015-01-06 16:13:00 -0500

    basic express app running, left off on page 66

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-var express = require('express');
+var express = require('./config/express');
+
 var app = express();
-
-app.use('/', function(req, res){
-	res.send('Hi World!');
-});
-
 app.listen(3000);
 console.log('Server up and running on port 3000.');
 module.exports = app;
